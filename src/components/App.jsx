56b1b0bb3c00e0314ac4ad00fa75dcc002fb0ffb
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-10 23:18:26 +0300

    t

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+import userData from './data/user.json';
+import statsData from './data/data.json';
+import friendData from './data/friends.json';
+import transactionData from './data/transactions.json'
+ 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        backgroundColor: 'grey',
+        paddingTop: '40px',
+        gap: '40px',
       }}
     >
-      React homework template
+      <Profile user={userData} />
+      <Statistics title="Upload stats" stats={statsData} />
+      <FriendList friends={friendData} />
+      <TransactionHistory transactions={transactionData} />
     </div>
   );
 };
