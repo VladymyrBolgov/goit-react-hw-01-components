0ec7fa4ece449e447bbe2a64e8551a016c675218
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-11 00:51:23 +0300

    t

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,26 +4,26 @@ import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => {
     return (
-        <section className={css.statistics}>
-            {title && <h2 className={css.title}>Upload stats</h2>}
-
-            <ul className={css.statList}>
-                {stats.map(stat => {
-                    return (
-                        <li 
-                            key={stat.id}
-                            className={css.item}
-                            style={{ backgroundColor: randomColor() }}
-                        >
-                            <span className={css.label}>{stat.label}</span>
-                            <span className={css.percentage}>{stat.percentage}</span>
-                        </li>
-                    );
-                })}
-            </ul>
-        </section>
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>Upload stats</h2>}
+  
+        <ul className={css.statList}>
+          {stats.map(stat => {
+            return (
+              <li
+                key={stat.id}
+                className={css.item}
+                style={{ backgroundColor: randomColor() }}
+              >
+                <span className={css.label}>{stat.label}</span>
+                <span className={css.percentage}>{stat.percentage}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     );
-};
+  };
 
 Statistics.propTypes = {
     title: PropTypes.string,
